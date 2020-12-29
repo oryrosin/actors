107da0e8d684511901df2b0d579c8025397359dd
Ory <orysjunk@gmail.com>
2020-12-29 10:52:55 +0200

    got some React-bs4 & Material

--- src/pages/ActorCard.js
+++ src/pages/ActorCard.js
@@ -1,21 +1,23 @@
 import "./ActorCard.css";
+import { Container, Row,  } from 'react-bootstrap';
+import Card from '@material-ui/core/Card';
 
 function ActorCard(props){
     const {actor}= props;
     
     return (
-    <div className="container"> 
-        <div className="row">
-            <div className="card ">
+    <Container>
+        <Row>
+            <Card>
                 <img className="card-img-top" src={actor.image} alt="Card " onClick={()=>window.open(actor.aLink) }/>
                 <div className="card-body">
                     <h4 className="card-title" style={{color:"black"}}>{actor.fName +" "+ actor.lName}</h4>
                     <p className="card-text" style={{color:"black"}}>{actor.age()}</p>
 
                 </div>
-            </div>
-        </div> 
-    </div>
+            </Card>
+        </Row> 
+        </Container>
     
         // <div className="card">
         //     <img src={actor.image} alt=""></img>
--- src/pages/ActorsPage.js
+++ src/pages/ActorsPage.js
@@ -14,11 +14,18 @@ function ActorsPage(props){
     const actor2= new ActorModel("Brad ","Pitt","1963-12-18",
         "https://m.media-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_UX214_CR0,0,214,317_AL_.jpg",
         "https://www.imdb.com/name/nm0000093/");
-    const actor3= new ActorModel ("David", "Bowie", "1947-01-08", 
+    const actor3= new ActorModel ("Tzipi", "Shavit", "1947-01-04", 
+        "https://m.media-amazon.com/images/M/MV5BNDM5N2RkNDctNDA3OS00OTgxLWIxODgtNzM2NWEzYTI3MGU4XkEyXkFqcGdeQXVyMjMyMzI4MzY@._V1_UY317_CR161,0,214,317_AL_.jpg",
+        "https://www.imdb.com/name/nm0789558/?ref_=nv_sr_srsg_0");
+
+    const actor4= new ActorModel ("David", "Bowie", "1947-01-08", 
         "https://m.media-amazon.com/images/M/MV5BMTQ4NTE3MTYzOF5BMl5BanBnXkFtZTcwNDM4OTcyMg@@._V1_UY317_CR7,0,214,317_AL_.jpg",
         "https://www.imdb.com/name/nm0000309/?ref_=fn_al_nm_1");
+    const actor5= new ActorModel ("Michelle", "Pfeiffer", "1958-04-29", 
+        "https://m.media-amazon.com/images/M/MV5BMTUzNjI0Njc5NF5BMl5BanBnXkFtZTYwOTM2MjYz._V1_UX214_CR0,0,214,317_AL_.jpg",
+        "https://www.imdb.com/name/nm0000201/?ref_=nv_sr_srsg_0");
     
-    const list= [actor1,actor2,actor3]; 
+    const list= [actor1,actor2,actor3,actor4, actor5]; 
     
     const filteredResult = list.filter(actor => actor.fName.toLowerCase().includes(filter.toLowerCase().trim()));
     const actorCard= filteredResult.map(actor=> <ActorCard actor={actor}/>);
