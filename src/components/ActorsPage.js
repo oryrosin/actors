3ff2fa1205db5a40d6b8656d3637fc6bfc4fd67a
ory <oooory@gmail.com>
2021-01-01 12:52:38 +0200

    load json

--- src/components/ActorsPage.js
+++ src/components/ActorsPage.js
@@ -1,36 +1,27 @@
 import React, { useEffect, useState } from "react";
 import ActorModel from '../models/ActorModel';
 import ActorCard from './ActorCard';
+import axios from 'axios';
 
 
 function ActorsPage(props){
     const {actors, selectedActors}=props;
     const [filter, setFilter]= useState("");
+    const [ actorsData, setActorsData]= useState([]);
+ 
+    useEffect(()=>{
+        axios.get("actors.json").then(res=>{
+        const actors= res.data.actors.map(plainActor => new ActorModel(plainActor));
+        setActorsData(actors)
+        });
+    });
+    let actorCard;
 
-    const actor1= new ActorModel(
-        "Daniel", "Day-Lewis","1957-04-29", 
-        "https://m.media-amazon.com/images/M/MV5BMjE2NDY2NDc1Ml5BMl5BanBnXkFtZTcwNjAyMjkwOQ@@._V1_UY317_CR13,0,214,317_AL_.jpg", 
-        "https://www.imdb.com/name/nm0000358/?ref_=nv_sr_srsg_0#actor" );
-    const actor2= new ActorModel("Brad ","Pitt","1963-12-18",
-        "https://m.media-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_UX214_CR0,0,214,317_AL_.jpg",
-        "https://www.imdb.com/name/nm0000093/");
-    const actor3= new ActorModel ("Tzipi", "Shavit", "1947-01-04", 
-        "https://m.media-amazon.com/images/M/MV5BNDM5N2RkNDctNDA3OS00OTgxLWIxODgtNzM2NWEzYTI3MGU4XkEyXkFqcGdeQXVyMjMyMzI4MzY@._V1_UY317_CR161,0,214,317_AL_.jpg",
-        "https://www.imdb.com/name/nm0789558/?ref_=nv_sr_srsg_0");
-
-    const actor4= new ActorModel ("David", "Bowie", "1947-01-08", 
-        "https://m.media-amazon.com/images/M/MV5BMTQ4NTE3MTYzOF5BMl5BanBnXkFtZTcwNDM4OTcyMg@@._V1_UY317_CR7,0,214,317_AL_.jpg",
-        "https://www.imdb.com/name/nm0000309/?ref_=fn_al_nm_1");
-    const actor5= new ActorModel ("Michelle", "Pfeiffer", "1958-04-29", 
-        "https://m.media-amazon.com/images/M/MV5BMTUzNjI0Njc5NF5BMl5BanBnXkFtZTYwOTM2MjYz._V1_UX214_CR0,0,214,317_AL_.jpg",
-        "https://www.imdb.com/name/nm0000201/?ref_=nv_sr_srsg_0");
-    
-    const list= [actor1,actor2,actor3,actor4, actor5]; 
-    
-    const filteredResult = list.filter(actor => actor.fName.toLowerCase().includes(filter.toLowerCase().trim()));
-    const actorCard= filteredResult.map(actor=> <ActorCard actor={actor}/>);
-
-
+    if (actorsData !== []) {
+        const filteredResult = actorsData.filter(actor => actor.fName.toLowerCase().includes(filter.toLowerCase()));
+        actorCard= filteredResult.map(actor=> <ActorCard actor={actor}/>);
+        
+    }
     return(
         <div id="main"> 
             <h1>Actors</h1>
@@ -43,7 +34,7 @@ function ActorsPage(props){
             
             <div className="container">
             
-                {actorCard}  
+                {actorCard!== undefined?  actorCard: "loading..." }  
             </div> 
         </div>
     )
--- src/models/ActorModel.js
+++ src/models/ActorModel.js
@@ -1,17 +1,27 @@
 class ActorModel{
-constructor(fName, lName,birthday, image, aLink){ 
-    this.fName= fName;
-    this.lName= lName;
-    this.birthday= new Date(birthday);
-    this.image= image;
-    this.aLink= aLink;
+constructor(plainActorOrFName, lName,birthday, image, aLink){ 
+    if(typeof plainActorOrFName=== 'object'){
+    this.fName= plainActorOrFName.fName;
+    this.lName= plainActorOrFName.lName;
+    this.birthday= plainActorOrFName.birthday;
+    this.image= plainActorOrFName.image;
+    this.aLink= plainActorOrFName.aLink;
+    } else{
+        this.fName= plainActorOrFName;
+        this.lName= lName;
+        this.birthday= birthday;
+        this.image= image;
+        this.aLink= aLink;
+    }
 }
 
+
     age(){
-        const diff = Date.now() - this.birthday.getTime();
+        const diff = Date.now() - new Date(this.birthday).getTime();
         const ageDate = new Date(diff);
         return Math.abs(ageDate.getUTCFullYear() - 1970);
     }
 }
 
+// const apiKey= "326d3ce51f35b38c9fc46926dc55bfaa";
 export default ActorModel
